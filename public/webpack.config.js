3c265a499fdcd70422fdd7ba6d81bb3fc6692c8f
Zuke27 <wstaylor27@gmail.com>
2022-01-08 20:47:39 -0500

    Added webpack

---
+++ public/webpack.config.js
@@ -0,0 +1,49 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./public/index.js",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: "Budget App",
+      short_name: "Budget",
+      description:
+        "An application that allows you to add deposits and expenses to your budget offline or online.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("icons/icon-192x192.png", "icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
